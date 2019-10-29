512636ebbf7bf2a847a9481217e503a163733edf
Andrew Matheus <andrewmatheus@gmail.com>
2019-10-29 14:26:59 -0300

    feito adição do avatar e configurações, iniciando os agendamentos

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,23 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
+routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
