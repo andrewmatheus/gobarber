dea9252985fb59b669041d6ccd8bdd1f043216bd
Andrew Matheus <andrewmatheus@gmail.com>
2019-10-30 17:05:52 -0300

    80% modulo 3 trabalhado agendamentos, cancelamente, envio de email, tratamento layout email.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9838ed8314fea0',
+    pass: 'a5690223e29cc0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
