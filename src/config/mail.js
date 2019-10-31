21cd57971cd4c501cb267f9287f73f43e8ec491a
Andrew Matheus <andrewmatheus@gmail.com>
2019-10-30 23:03:23 -0300

    Finalizaçao do modulo 3 partir desafio 3

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '9838ed8314fea0',
-    pass: 'a5690223e29cc0',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
